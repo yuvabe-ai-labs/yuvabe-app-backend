cc8f8080033bf94cee54685eaf050b31f1422a0e
Shri <shri@shri.s-space>
2025-11-06 23:10:42 +0530

    added req/response model

--- src/home/router.py
+++ src/home/router.py
@@ -3,7 +3,7 @@ from sqlmodel import Session
 
 from src.core.database import engine
 
-from .schemas import EmotionLogCreate, EmotionLogResponse, HomeResponse
+from .schemas import BaseResponse, EmotionLogCreate
 from .service import add_or_update_emotion, get_emotions, get_home_data
 
 router = APIRouter(prefix="/home", tags=["Home"])
@@ -14,26 +14,31 @@ def get_session():
         yield session
 
 
-@router.get("/{user_id}", response_model=HomeResponse)
+@router.get("/{user_id}", response_model=BaseResponse)
 def fetch_home_data(user_id: str, session: Session = Depends(get_session)):
     try:
-        return get_home_data(user_id, session)
+        data = get_home_data(user_id, session)
+        return {"code": 200, "data": data}
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
 
 
-@router.post("/emotion", response_model=EmotionLogResponse)
+@router.post("/emotion", response_model=BaseResponse)
 def create_or_update_emotion(
     data: EmotionLogCreate, session: Session = Depends(get_session)
 ):
     record = add_or_update_emotion(data, session)
-    return EmotionLogResponse(
-        log_date=record.log_date,
-        morning_emotion=record.morning_emotion,
-        evening_emotion=record.evening_emotion,
-    )
+    return {
+        "code": 200,
+        "data": {
+            "log_date": record.log_date,
+            "morning_emotion": record.morning_emotion,
+            "evening_emotion": record.evening_emotion,
+        },
+    }
 
 
-@router.get("/emotion/{user_id}", response_model=list[EmotionLogResponse])
+@router.get("/emotion/{user_id}", response_model=BaseResponse)
 def get_user_emotions(user_id: str, session: Session = Depends(get_session)):
-    return get_emotions(user_id, session)
+    data = get_emotions(user_id, session)
+    return {"code": 200, "data": data}
