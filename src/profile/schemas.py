a614e8acd833994755d797add60572860ad59c68
Hariprasath137 <hariprasath@yuvabe.com>
2025-11-17 20:21:07 +0530

    chore:merge leave and assets in profile

--- src/profile/schemas.py
+++ src/profile/schemas.py
@@ -1,5 +1,35 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
+import uuid
+from enum import Enum
+
+class AssetStatus(str, Enum):
+    ACTIVE = "Active"
+    UNAVAILABLE = "Unavailable"
+    ON_REQUEST = "On Request"
+    IN_SERVICE = "In Service"
+
+class AssetCreateRequest(BaseModel):
+    name: str
+    type: str
+    status: Optional[AssetStatus] = AssetStatus.UNAVAILABLE
+
+class AssetUpdateRequest(BaseModel):
+    name: Optional[str] = None
+    type: Optional[str] = None
+    status: Optional[AssetStatus] = None
+
+class AssetResponse(BaseModel):
+    id: uuid.UUID
+    user_id: uuid.UUID
+    name: str
+    type: str
+    status: AssetStatus
+
+class BaseResponse(BaseModel):
+    code: int
+    data: dict
+
 
 class UpdateProfileRequest(BaseModel):
     name: Optional[str] = None
