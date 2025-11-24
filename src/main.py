ce08d0aa6a2746af8b2b2caebf3be3d829f05a79
Shri <shri@shri.s-space>
2025-11-24 08:56:02 +0530

    feat:audio file added

--- src/main.py
+++ src/main.py
@@ -1,23 +1,27 @@
-# from src.profile.router import router as profile
 from fastapi import FastAPI
 
-
+import os
 from src.auth.router import router as auth_router
 from src.chatbot.router import router as chatbot
 from src.core.database import init_db
 from src.home.router import router as home_router
+from fastapi.staticfiles import StaticFiles
 
-# from src.profile.router import router as profile
 
 app = FastAPI(title="Yuvabe App API")
 
+
 app.include_router(home_router, prefix="/home", tags=["Home"])
 
-init_db()
+# init_db()
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+AUDIO_DIR = os.path.join(BASE_DIR, "static", "audio")
+
+app.mount("/static/audio", StaticFiles(directory=AUDIO_DIR), name="audio")
+print("Serving audio from:", AUDIO_DIR)
 
 app.include_router(auth_router)
-
-# app.include_router(profile)
 
 app.include_router(chatbot)
 
