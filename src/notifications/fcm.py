42fb14b7461df98e4e9ff5a8e7d4ac908adbe00f
Hariprasath137 <hariprasath@yuvabe.com>
2025-11-26 14:55:46 +0530

    updated config

--- src/notifications/fcm.py
+++ src/notifications/fcm.py
@@ -1,24 +1,33 @@
-import os
-import json
+from src.core.config import settings
 import httpx
 from google.oauth2 import service_account
 import google.auth.transport.requests
 
 # Your Firebase project ID (from project settings)
-FCM_PROJECT_ID = "yuvabe-478505"  # <-- change this
+FCM_PROJECT_ID = settings.FIREBASE_PROJECT_ID  # <-- change this
 
-# Path to your service account file
-SERVICE_ACCOUNT_FILE = os.path.join(
-    os.path.dirname(__file__), "yuvabe-478505-09433c5e6e33.json"
-)
+
+service_account_info = {
+    "type": settings.FIREBASE_TYPE,
+    "project_id": settings.FIREBASE_PROJECT_ID,
+    "private_key_id": settings.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": settings.FIREBASE_PRIVATE_KEY.replace("\\n", "\n"),
+    "client_email": settings.FIREBASE_CLIENT_EMAIL,
+    "client_id": settings.FIREBASE_CLIENT_ID,
+    "auth_uri": settings.FIREBASE_AUTH_URI,
+    "token_uri": settings.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": settings.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": settings.FIREBASE_CLIENT_X509_CERT_URL,
+    "universe_domain": settings.FIREBASE_UNIVERSE_DOMAIN,
+}
 
 
 def get_access_token():
     """Generate OAuth2 access token for FCM HTTP v1."""
     scopes = ["https://www.googleapis.com/auth/firebase.messaging"]
 
-    credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=scopes
+    credentials = service_account.Credentials.from_service_account_info(
+        service_account_info, scopes=scopes
     )
 
     request = google.auth.transport.requests.Request()
